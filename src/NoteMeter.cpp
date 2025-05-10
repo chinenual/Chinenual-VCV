d83670d093f944de13a13e5fa03698f879b2ecfc
Steve Tynor <chinenual@gmail.com>
2025-05-10 07:40:42 -0400

    new mode option in notemeter was not persisted properly

--- src/NoteMeter.cpp
+++ src/NoteMeter.cpp
@@ -59,7 +59,7 @@ namespace NoteMeter {
                 configInput(i, string::f("Pitch %d", i - PITCH_INPUT_1 + 1));
             }
             configParam(NOTE_ACCIDENTAL_PARAM, 0.f, 1.f, 0.f, "Display notes as sharps or flats");
-            configParam(VOLTAGE_MODE_PARAM, 0.f, 1.f, 0.f, "Display voltage value rather than note name");
+            configParam(VOLTAGE_MODE_PARAM, 0.f, 2.f, 0.f, "Display voltage value rather than note name");
             configParam(VOLTAGE_DECIMALS_PARAM, 0.f, 8.f, 6.f, "Number of decimal places to display in voltage value");
         }
 
